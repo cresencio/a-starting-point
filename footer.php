240f59936546e2bc980ee3cb3355e1501cdff41d
Cresencio Cantu <holler@cresenc.io>
2019-03-20 20:02:04 -0500

    Add grid structure to header and footer

--- footer.php
+++ footer.php
@@ -10,23 +10,30 @@
  */
 
 ?>
-
+			</div> <!-- .row -->
+		</div> <!-- .container-fluid -->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'asp-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'asp-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'asp-theme' ), 'asp-theme', '<a href="https://cresenc.io">@cresencio</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col">
+					<div class="site-info">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'asp-theme' ) ); ?>">
+							<?php
+							/* translators: %s: CMS name, i.e. WordPress. */
+							printf( esc_html__( 'Proudly powered by %s', 'asp-theme' ), 'WordPress' );
+							?>
+						</a>
+						<span class="sep"> | </span>
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Theme: %1$s by %2$s.', 'asp-theme' ), 'asp-theme', '<a href="https://cresenc.io">@cresencio</a>' );
+							?>
+					</div><!-- .site-info -->
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
