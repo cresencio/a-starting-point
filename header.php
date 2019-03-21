9c8ecb486e22c05dbf624cb5d563e021a58323d4
Cresencio Cantu <holler@cresenc.io>
2019-03-20 19:38:31 -0500

    Update header.php
    - add sr-only class so it doesn't show

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'asp-theme' ); ?></a>
+	<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'asp-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
