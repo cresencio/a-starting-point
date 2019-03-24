4d5b12c0088e50dd181d1e8e7c43f11aa7181b69
Cresencio Cantu <holler@cresenc.io>
2019-03-24 08:45:22 -0500

    Update header.php
    - remove the hard coded menu class stuff

--- header.php
+++ header.php
@@ -54,12 +54,9 @@
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'asp-theme' ); ?></button>
 						<?php
 
-						$custom_menu_class = get_field('asp_custom_menu_class', wp_get_nav_menu_object('all-pages'));
-
 						wp_nav_menu( array(
 							'theme_location' => 'menu-1',
 							'menu_id'        => 'primary-menu',
-							'menu_class'     => ( $custom_menu_class ) ? $custom_menu_class : 'foo',
 						) );
 
 						?>
