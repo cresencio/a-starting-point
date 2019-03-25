601e76b39532c8b777a4607a1756ae58e78d8388
Cresencio Cantu <holler@cresenc.io>
2019-03-25 18:14:31 -0500

    Update advanced-custom-fields.php
    - remove the included acf code and instead add the plugin through the dashboard

--- inc/advanced-custom-fields.php
+++ inc/advanced-custom-fields.php
@@ -1,33 +1,7 @@
 <?php
 
-// 1. customize ACF path
-add_filter('acf/settings/path', 'asp_theme_acf_settings_path');
 
-function asp_theme_acf_settings_path( $path ) {
-
-    // update path
-    $path = get_stylesheet_directory() . '/inc/advanced-custom-fields/';
-
-    // return
-    return $path;
-
-}
-
-
-// 2. customize ACF dir
-add_filter('acf/settings/dir', 'asp_theme_acf_settings_dir');
-
-function asp_theme_acf_settings_dir( $dir ) {
-
-    // update path
-    $dir = get_stylesheet_directory_uri() . '/inc/advanced-custom-fields/';
-
-    // return
-    return $dir;
-
-}
-
-// 3. add acf-json path
+// add acf-json path
 add_filter('acf/settings/save_json', 'asp_theme_acf_json_save_point');
 
 function asp_theme_acf_json_save_point( $path ) {
@@ -57,6 +31,5 @@ function asp_theme_json_load_point( $paths ) {
 //add_filter('acf/settings/show_admin', '__return_false');
 
 // 5. Include ACF
-include_once( get_stylesheet_directory() . '/inc/advanced-custom-fields/acf.php' );
 
 ?>
