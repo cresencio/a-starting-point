e2d7e843ec87536f2d5277a5e3fb87dc5d4b65c8
Cresencio Cantu <holler@cresenc.io>
2019-04-11 19:39:55 -0500

    Update header.php
    - remove font awesome
    - remove span from button

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
 
@@ -24,6 +23,7 @@
 
 <body <?php body_class($sidebar_position); ?>>
 <div id="page" class="site">
+
 	<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'asp-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
@@ -35,25 +35,34 @@
 				<!-- <div class="col"> -->
 					<div class="site-branding">
 						<?php
+
 						the_custom_logo();
-						if ( is_front_page() && is_home() ) :
-							?>
+
+						if ( is_front_page() && is_home() ) : ?>
+
 							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
 							<?php
-						else :
-							?>
+						else : ?>
+
 							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+
 							<?php
 						endif;
+
 						$asp_theme_description = get_bloginfo( 'description', 'display' );
-						if ( $asp_theme_description || is_customize_preview() ) :
-							?>
+
+						if ( $asp_theme_description || is_customize_preview() ) : ?>
+
 							<p class="site-description"><?php echo $asp_theme_description; /* WPCS: xss ok. */ ?></p>
+
 						<?php endif; ?>
+
 					</div><!-- .site-branding -->
 
 					<nav id="site-navigation" class="main-navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="sr-only"><?php esc_html_e( 'Primary Menu', 'asp-theme' ); ?></span>menu toggle</button>
+
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'menu toggle', 'asp-theme' ); ?></button>
 						<?php
 
 						wp_nav_menu( array(
