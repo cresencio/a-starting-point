c6b6ea32af33dca3b6fd1e458fcfedee759852ea
Cresencio Cantu <cresencioc@smu.edu>
2019-04-25 09:35:43 -0500

    Update header.php
    - add wp_open_body() to the opening body tag

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
 <div id="page" class="site">
 
 	<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'asp-theme' ); ?></a>
